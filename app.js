4a0798d04c1198145e6cfa4280a619788c10a5d9
alunia1225-cell <alunia1225@gmail.com>
2026-08-22 16:55:11 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ window.addEventListener("unhandledrejection",e=>debugLog("ERROR","UNHANDLED PROM
 window.addEventListener("DOMContentLoaded",()=>{
   const t=document.getElementById("debugToggle");
   if(t)t.addEventListener("click",toggleDebug);
-  debugLog("BOOT","DEBUG RUNTIME ONLINE",{version:"4.7.61"});
+  debugLog("BOOT","DEBUG RUNTIME ONLINE",{version:"4.7.62"});
 });
 
 const KEY="gb3_save";
